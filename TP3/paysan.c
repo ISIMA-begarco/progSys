d09b0aa7474862dc0a0ab239d4140ce3e81ce0b4
Benoît Garçon <begarco@Debius>
2016-01-31 10:44:11 +0100

    Paysan ended

--- TP3/paysan.c
+++ TP3/paysan.c
@@ -13,48 +13,62 @@ int main() {
 
 	int fils[NOMBRE_FILS] = {0,0};
 	int pere = getpid();
-	int fd[2][2];
+	int fd[3][2];
 	int result = 0;
 	pipe(fd[0]);
 	pipe(fd[1]);
+	pipe(fd[2]);
 	
-	for(int i = 0 ; i < NOMBRE_FILS ; ++i) {
-		fils[i] = fork();
-		if(!fils[i]) {
-			close(fd[i][1]);
-			if(i==0) {
-				printf("Fils %d: Je suis le fils %d (le salaud).\n", getpid(), getpid());
-			} else {
-				printf("Fils %d: Je suis le fils %d (le vengeur).\n", getpid(), getpid());
-			}
+	/** FILS AINE MEURTRIER */
+	fils[0] = fork();
+	if(!fils[0]) {
+		close(fd[0][1]);
+		close(fd[1][1]);
+		printf("Fils %d: Je suis le fils %d (le salaud).\n", getpid(), getpid());
+		fflush(stdout);
 
-			fflush(stdout);
-
-			read(fd[i][0], &result, sizeof(result));	//lecture
-			
-			if(1==i)
-				pere = fils[0];		
-			
-			printf("Fils %d: Enfoire ! Meurs !\n", getpid());
-			fflush(stdout);
-			kill(pere, SIGTERM);			
-			pause();			
-			
-
-			return 0;
-		}
+		read(fd[0][0], &result, sizeof(result));	//lecture
+		
+		printf("Fils %d: Enfoire ! Meurs !\n", getpid());
+		fflush(stdout);
+		kill(pere, SIGKILL);
+		pause();			
+		
+		return 0;
 	}
-close(fd[0][0]);
-close(fd[1][0]);
+	
+	/** FILS CADET VENGEUR */
+	fils[1] = fork();
+	if(!fils[1]) {
+		close(fd[0][1]);
+		close(fd[1][1]);
+		printf("Fils %d: Je suis le fils %d (le vengeur).\n", getpid(), getpid());
+
+		fflush(stdout);
+		
+		/* le dernier fils est ne */
+		close(fd[2][0]);	//libere la lecture
+		write(fd[2][1], &result, sizeof(result));	//ecriture
+		read(fd[1][0], &result, sizeof(result));	//lecture	
+		
+		printf("Fils %d: Enfoire ! Meurs !\n", getpid());
+		fflush(stdout);
+		kill(fils[0], SIGKILL);
+		printf("Fils %d: Adieu monde cruel !\n", getpid());
+		kill(fils[1], SIGKILL);
+
+		return 0;
+	}
+	
+	close(fd[2][1]);	// liberer l'ecriture
+	close(fd[0][0]);	//libere la lecture
+	close(fd[1][0]);	//libere la lecture
+	read(fd[2][0], &result, sizeof(result));	//lecture	
 	printf("PERE: Fils %d, j'ai tue ta mere !\n", fils[0]);
 	fflush(stdout);
-	close(fd[0][0]);	//libere la lecture
 	write(fd[0][1], &result, sizeof(result));	//ecriture
-
-	for(int i = 0 ; i < NOMBRE_FILS ; ++i) {
-		close(fd[i][0]);	//libere la lecture
-		//read(fd[i][0], &result, sizeof(result));	//lecture
-	}
-pause();
+	
+	pause();
+	
 	return 0;
 }
