6d1ddd5736dfb143ca2e15c970ff28bc45d71b8a
begarco <benoit.garconbesson@gmail.com>
2016-02-02 20:58:20 +0100

    f

--- README.md
+++
@@ -1,4 +0,0 @@
-# Dépôt des TP du cours de prgrammation système
-
-## Premier TP
-Le TP1 porte sur l'utilisation des forks de processus.
--- old_TP1/image.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from gi.repository import Gtk
-
-window = Gtk.Window()
-window.connect_after('destroy', Gtk.main_quit)
-window.add(Gtk.Image.new_from_file('/dev/stdin'))
-window.show_all()
-Gtk.main()
