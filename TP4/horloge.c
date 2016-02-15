8c3714350565bec504bbc4c4577cd36e36889406
Benoît Garçon <benoit.garcon@outlook.com>
2016-02-15 23:27:22 +0100

    Exercice 2 termine

--- TP4/horloge.c
+++ TP4/horloge.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>     
@@ -10,137 +12,113 @@
 #include <sys/sem.h>
 
 
-#define	NBFILS	4
-#define	HMAX	2
-#define	MMAX	6
-#define	SMAX	6
+#define	NBFILS	5
+#define	HMAX	24
+#define	MMAX	60
+#define	SMAX	60
 
-int heure = 0;
-int pere = 0;
-int result = 0;
 int fils[NBFILS];
-int fd[3][2];
+int sem = 0;
 
-void display1(int s);
-void display2(int s);
-void display3(int s);
-void reset(int s);
-void inc1(int s);
-void inc2(int s);
+void safeExit(int s);
 
 int main() {
 	time_t t[2];
 	int choix = 1;
-	pipe(fd[0]);
-	pipe(fd[1]);
-	pipe(fd[2]);
 
-	int sem = semget(IPC_PRIVATE, 3, 0600); // création
-	unsigned short values[] = {SMAX, MMAX, HMAX};
-	semctl(sem, 0, SETALL, values);	// initialisation
+	sem = semget(ftok("./semtest", 1103), 3, 0660 | IPC_CREAT);             // création
+	unsigned short values[] = {0, 0, 0};
+	semctl(sem, 0, SETALL, values);	                    // initialisation
+	struct sembuf cmd[] = {{0,+1,0},{0,-SMAX,0},{1,+1,0},{1,-MMAX,0},{2,+1,0},{2,-HMAX,0}};   // commande pour operation
 	
-	pere = getpid();
+	signal(SIGTERM, safeExit);      // quitter proprement
+	signal(SIGINT, safeExit);       // quoi qu'il arrive
 	
 	for(int i = 0; i  < NBFILS ; i++) {
 		fils[i] = fork();
 		if(!fils[i]) {	/// les fils
 			if(i==0) { 		/// heures
-				signal(SIGUSR1, display1);
-				signal(SIGUSR2, reset);
-				signal(SIGALRM, inc1);
-				close(fd[0][1]);	// fermeture ecriture
+			
+				int heure = 0;
 				while(1) {
-					close(fd[0][1]);	// fermeture ecriture
-					result = 0;
-					read(fd[0][0], &result, sizeof(result));	//lecture
-					if(result==5) {
-						heure++;
-						if(heure>=HMAX) {
-							heure=0;
-						}
-					}
+				
+				    semop(sem, cmd+3, 1);	// decrementation non blocante
+			        semop(sem, cmd+4, 1);	// incrementation non blocante
+				    heure++;
+				    if(heure>=HMAX) {
+				        semop(sem, cmd+5, 1);	// decrementation non blocante
+				        heure = 0;
+				    }
+					
 				}
+				
 			} else if(i==1) {	/// minutes
-				signal(SIGUSR1, display2);
-				signal(SIGUSR2, reset);
-				signal(SIGALRM, inc2);
-				heure=0;
-				close(fd[0][0]);	// fermeture lecture
-				close(fd[1][1]);	// fermeture ecriture
+			
 				while(1) {
-					struct sembuf cmd[] = {{0,0,0},{1,+1,0},{2,0,0}};
-					semop(sem, &cmd, 0);	// attente
-					heure++;
-					semop(sem, &cmd+1, 1);	// attente
-					if(heure>=MMAX) {
-						heure=0;
-					}
+				
+				    semop(sem, cmd+1, 1);	// decrementation non blocante
+					semop(sem, cmd+2, 1);	// incrementation non blocante
+										
 				}
+				
 			} else if(i==2) {	/// secondes
-				signal(SIGUSR1, display3);
-				signal(SIGUSR2, reset);
-				signal(SIGALRM, inc2);
-				heure=0;
+			
 				time(&t[0]);
 				time(&t[1]);
-				close(fd[1][0]);	// fermeture lecture
-				struct sembuf cmd[] = {{0,+1,0},{1,0,0},{2,0,0}};
 				while(1) {
+				
 					while(1.00 > difftime(t[1],t[0])) {
 						time(&t[1]);
 					}
-					heure++;
 					t[0] = t[1];
-					semop(sem, &cmd, 0);	// increment
-					semctl(sem, 0, GETALL, values);
-					printf("GETALL–> %d %d %d\n", values[0], values[1], values[2]);
-					if(heure>=SMAX) {
-						heure=0;
-						semctl(sem, 0, SETVAL, -SMAX);
-					}
+					semop(sem, cmd+0, 1);	// incrementation non blocante
+
 				}
-			} else if(i==3) {
+				
+			} else if(i==3) {   // programme pour bip
+			
+		        unsigned short valeur = 0, old = 0;
+                while(1) {
+                
+			        semctl(sem, 0, GETALL, values);
+			        valeur = values[2];
+			        if(valeur != old) {
+					    fflush(stdout);
+    			        for(unsigned short incBip = 0 ; incBip < valeur ; incBip++) {
+	    		            printf("\aBIP ");
+					        fflush(stdout);
+	    		        }
+	    		        printf("\n");
+					    fflush(stdout);
+	    		        old = valeur;
+	    		    }
+	    		    
+			    }
+			
+			} else if(i==4) {
+			
 				while(choix) {
+				
 					puts("Horloge Suisse\n--------------\n");
 					puts("1 - Afficher l'heure");
-					puts("2 - Incrementer les secondes");
-					puts("3 - Incrementer les minutes");
-					puts("4 - Incrementer les heures");
-					puts("5 - Reset");
 					puts("0 - Quitter");
 					fflush(stdout);
-					scanf("%d", &choix);
+					scanf("%d%*c", &choix);
 
 					if(1==choix) {
-						printf("Il est : ");
+					
+						semctl(sem, 0, GETALL, values);
+					    printf("Il est : %02d:%02d:%02d\n\n", values[2], values[1], values[0]);
 						fflush(stdout);
-						for(int j = 0 ; j < 3 ; ++j) {
-							kill(fils[j], SIGUSR1);
-							fflush(stdout);
-							close(fd[2][1]);	// fermeture ecriture
-							read(fd[2][0], &result, sizeof(result));	//lecture
-						}
-					} else if(2==choix) {
-						kill(fils[2], SIGALRM);
-						puts("Secondes incrementees.\n");
-					} else if(3==choix) {
-						kill(fils[1], SIGALRM);
-						puts("Minutes incrementees.\n");
-					} else if(4==choix) {
-						kill(fils[0], SIGALRM);
-						puts("Heures incrementees.\n");
-					} else if(5==choix) {
-						for(int j = 0 ; j < 3 ; ++j) {
-							kill(fils[j], SIGUSR2);
-						}
-						puts("Horloge remise a zero.\n");
+						
 					} else if(0==choix) {
-						kill(pere, SIGTERM);
-						for(int j = 0 ; j < NBFILS ; j++) {
-							kill(fils[j], SIGTERM);
-						}
+					
+					    safeExit(0);
+					    
 					}
+					
 				}
+				
 			}
 			return 0;
 		}
@@ -151,42 +129,16 @@ int main() {
 	return 0;
 }
 
-
-void display1(int s) {
-	printf("%02d:", heure);
-	fflush(stdout);
-	signal(SIGUSR1, display1);
-	close(fd[2][0]);	// fermeture lecture
-	write(fd[2][1], &result, sizeof(result));	//ecriture
-}
-
-void display2(int s) {
-	printf("%02d:", heure);
-	fflush(stdout);
-	signal(SIGUSR1, display2);
-	close(fd[2][0]);	// fermeture lecture
-	write(fd[2][1], &result, sizeof(result));	//ecriture
-}
-
-void display3(int s) {
-	printf("%02d\n\n", heure);
-	fflush(stdout);
-	signal(SIGUSR1, display3);
-	close(fd[2][0]);	// fermeture lecture
-	write(fd[2][1], &result, sizeof(result));	//ecriture
-}
-
-void reset(int s) {
-	heure = 0;
-	signal(SIGUSR2, reset);
-}
-
-void inc1(int s) {
-	heure = heure+1>=HMAX?0:heure+1;
-	signal(SIGALRM, inc1);
-}
-
-void inc2(int s) {
-	heure = heure+1>=SMAX?0:heure+1;
-	signal(SIGALRM, inc2);
+void safeExit(int s) {
+    s = s;
+    for(int i = 0 ; i < NBFILS ; i++) {
+        kill(fils[i], SIGTERM);
+    }
+    if(semctl(sem, 0, IPC_RMID) == -1) {
+        perror("Erreur lors de la suppression du tableau de semaphores ");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("SEMAPHORE LIBERE\n");
+    }
+    exit(EXIT_SUCCESS);
 }
